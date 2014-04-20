9ac275276eeec8a7dcd27f2ccd05d09778d3a9d4
Linus Oleander <linus@oleander.nu>
2014-04-21 01:37:02 +0200

    Remove debug message

--- tests/GooglTest.php
+++ tests/GooglTest.php
@@ -2,7 +2,6 @@
 
 class GooglTest extends PHPUnit_Framework_TestCase {
   public function testUrlShorten() {
-    echo $_ENV["USERNAME"];
     $client = new Googl($_ENV["USER"], $_ENV["PASSWORD"]);
     $short = $client->shorten("http://www.bbc.co.uk/");
     $this->assertNotNull($short["id"]);
